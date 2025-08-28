0ad46793d8b0b053cc4b11dccd80a1239b047722
Jenner369 <64997441+Jenner369@users.noreply.github.com>
2025-08-28 16:38:44 -0500

    fix: Add correct migrations

--- applications/app-service/src/main/resources/db/migration/V1__init.sql
+++ applications/app-service/src/main/resources/db/migration/V1__init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE statuses (
-                          id CHAR(36) PRIMARY KEY,
+                          code VARCHAR(50) PRIMARY KEY,
                           name VARCHAR(255) NOT NULL,
                           description TEXT
 );
 
 
 CREATE TABLE loan_types (
-                            id CHAR(36) PRIMARY KEY,
+                            code VARCHAR(50) PRIMARY KEY,
                             name VARCHAR(255) NOT NULL,
                             min_amount NUMERIC(15,2) NOT NULL,
                             max_amount NUMERIC(15,2) NOT NULL,
@@ -20,6 +20,6 @@ CREATE TABLE loan_applications (
                                    term INT NOT NULL,
                                    identity_document VARCHAR(50) NOT NULL,
                                    email VARCHAR(255) NOT NULL,
-                                   status_id CHAR(36) NOT NULL REFERENCES statuses(id),
-                                   loan_type_id CHAR(36) NOT NULL REFERENCES loan_types(id)
+                                   status_code VARCHAR(50) NOT NULL REFERENCES statuses(code),
+                                   loan_type_code VARCHAR(50) NOT NULL REFERENCES loan_types(code)
 );
--- applications/app-service/src/main/resources/db/migration/V2__insert_statuses.sql
+++ applications/app-service/src/main/resources/db/migration/V2__insert_statuses.sql
@@ -1,4 +1,4 @@
-INSERT INTO statuses (id, name, description) VALUES
-                                                 ('828ca174-c4a7-4850-b759-996219edef6c', 'Pendiente de Revisión', 'Solicitud en espera de ser revisada por un asesor'),
-                                                 ('0a00524c-b32e-47e3-94d1-615461984428', 'Aprobado', 'Cliente aprobado para el préstamo'),
-                                                 ('44468889-fac0-424f-abf6-f52424aaad09', 'Rechazado', 'Cliente no aprobado para el préstamo');
+INSERT INTO statuses (code, name, description) VALUES
+                                                 ('pending', 'Pendiente de Revisión', 'Solicitud en espera de ser revisada por un asesor'),
+                                                 ('approved', 'Aprobado', 'Cliente aprobado para el préstamo'),
+                                                 ('rejected', 'Rechazado', 'Cliente no aprobado para el préstamo');
