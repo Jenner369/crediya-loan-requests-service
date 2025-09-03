b399ad6f7f44a5e245d00e2c4c3aca63f21a6a07
Jenner369 <64997441+Jenner369@users.noreply.github.com>
2025-09-03 17:32:56 -0500

    feat: Add reactive repository for loan applications

--- infrastructure/driven-adapters/r2dbc-mysql/src/main/java/co/com/crediya/r2dbc/adapter/LoanApplicationReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-mysql/src/main/java/co/com/crediya/r2dbc/adapter/LoanApplicationReactiveRepositoryAdapter.java
@@ -2,11 +2,17 @@ package co.com.crediya.r2dbc.adapter;
 
 import co.com.crediya.model.loanapplication.LoanApplication;
 import co.com.crediya.model.loanapplication.gateways.LoanApplicationRepository;
+import co.com.crediya.model.loantype.LoanType;
+import co.com.crediya.model.status.Status;
+import co.com.crediya.model.status.enums.Statuses;
 import co.com.crediya.r2dbc.entity.LoanApplicationEntity;
+import co.com.crediya.r2dbc.projection.LoanApplicationWithDetails;
 import co.com.crediya.r2dbc.repository.LoanApplicationReactiveRepository;
 import co.com.crediya.r2dbc.helper.ReactiveAdapterOperations;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
@@ -22,4 +28,41 @@ public class LoanApplicationReactiveRepositoryAdapter extends ReactiveAdapterOpe
         super(repository, mapper, d -> mapper.map(d, LoanApplication.class));
     }
 
+    @Override
+    public Flux<LoanApplication> findAllWithDetails(
+            String identityDocument,
+            String statusCode,
+            String loanTypeCode,
+            Boolean autoApproval,
+            Integer pageNumber,
+            Integer pageSize
+    ) {
+        var offset = (pageNumber - 1) * pageSize;
+
+        return repository.findAllWithDetails(
+                        identityDocument,
+                        statusCode,
+                        loanTypeCode,
+                        Statuses.APPROVED.getCode(),
+                        autoApproval,
+                        pageSize,
+                        offset
+                )
+                .map(LoanApplicationWithDetails::toModel);
+    }
+
+    @Override
+    public Mono<Long> countByFilters(
+            String identityDocument,
+            String statusCode,
+            String loanTypeCode,
+            Boolean autoApproval
+    ) {
+        return repository.countByFilters(
+                identityDocument,
+                statusCode,
+                loanTypeCode,
+                autoApproval
+        );
+    }
 }
--- infrastructure/driven-adapters/r2dbc-mysql/src/test/java/co/com/crediya/r2dbc/LoanApplicationReactiveRepositoryAdapterTest.java
+++ infrastructure/driven-adapters/r2dbc-mysql/src/test/java/co/com/crediya/r2dbc/LoanApplicationReactiveRepositoryAdapterTest.java
@@ -3,8 +3,10 @@ package co.com.crediya.r2dbc;
 import co.com.crediya.model.loanapplication.LoanApplication;
 import co.com.crediya.model.loantype.enums.LoanTypes;
 import co.com.crediya.model.status.enums.Statuses;
+import co.com.crediya.model.user.User;
 import co.com.crediya.r2dbc.adapter.LoanApplicationReactiveRepositoryAdapter;
 import co.com.crediya.r2dbc.entity.LoanApplicationEntity;
+import co.com.crediya.r2dbc.projection.LoanApplicationWithDetails;
 import co.com.crediya.r2dbc.repository.LoanApplicationReactiveRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,10 +14,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.reactivecommons.utils.ObjectMapper;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -42,8 +46,26 @@ class LoanApplicationReactiveRepositoryAdapterTest {
                     "12345678",
                     "jennerjose369@gmail.com",
                     Statuses.PENDING.getCode(),
-                    LoanTypes.BUSINESS_LOAN.getCode()
+                    LoanTypes.BUSINESS_LOAN.getCode(),
+                    LocalDateTime.now()
             );
+
+    private final LoanApplicationWithDetails sampleLoanApplicationProjection =
+            new LoanApplicationWithDetails(
+                    sampleId,
+                    new BigDecimal("1000000"),
+                    12,
+                    "12345678",
+                    "jennerjose369@gmail.com",
+                    Statuses.PENDING.getCode(),
+                    Statuses.PENDING.getName(),
+                    Statuses.PENDING.getDescription(),
+                    LoanTypes.BUSINESS_LOAN.getCode(),
+                    LoanTypes.BUSINESS_LOAN.getName(),
+                    LoanTypes.BUSINESS_LOAN.getInterestRate(),
+    null
+            );
+
 
     private final LoanApplication sampleLoanApplication =
             new LoanApplication(
@@ -53,7 +75,17 @@ class LoanApplicationReactiveRepositoryAdapterTest {
                     "12345678",
                     "jennerjose369@gmail.com",
                     Statuses.PENDING.getCode(),
-                    LoanTypes.BUSINESS_LOAN.getCode()
+                    LoanTypes.BUSINESS_LOAN.getCode(),
+                    LoanTypes.BUSINESS_LOAN.toModel(),
+                    Statuses.PENDING.toModel(),
+                    User.builder()
+                            .name("Jenner")
+                            .lastName("Durand")
+                            .email("jennerjose369@gmail.com")
+                            .identityDocument("jennerjose369@gmail.com")
+                            .baseSalary(new BigDecimal("5000000"))
+                            .build(),
+                    null
             );
 
     @Test
@@ -76,6 +108,47 @@ class LoanApplicationReactiveRepositoryAdapterTest {
 
         StepVerifier.create(result)
                 .expectNextMatches(value -> value.equals(sampleLoanApplication))
+                .verifyComplete();
+    }
+
+    @Test
+    void mustFindAllWithDetails() {
+        when(repository.findAllWithDetails(
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                eq(1),
+                eq(0)
+        )).thenReturn(Flux.just(sampleLoanApplicationProjection));
+
+        var result = repositoryAdapter.findAllWithDetails(
+                "12345678",
+                Statuses.PENDING.getCode(),
+                LoanTypes.BUSINESS_LOAN.getCode(),
+                false,
+                1,
+                1
+        );
+
+        StepVerifier.create(result)
+                .expectNextMatches(value ->
+                        value.getIdentityDocument()
+                                .equals(sampleLoanApplication.getIdentityDocument())
+                )
+                .verifyComplete();
+    }
+
+    @Test
+    void mustCountByFilters() {
+        when(repository.countByFilters(any(), any(), any(), any()))
+                .thenReturn(Mono.just(5L));
+
+        var result = repositoryAdapter.countByFilters("12345678", null, null, false);
+
+        StepVerifier.create(result)
+                .expectNext(5L)
                 .verifyComplete();
     }
 }
