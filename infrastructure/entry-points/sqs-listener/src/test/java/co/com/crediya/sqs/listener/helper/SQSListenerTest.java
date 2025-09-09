422999c5e6637a8fa61f2df8eab3204302baaf29
Jenner369 <64997441+Jenner369@users.noreply.github.com>
2025-09-08 19:04:11 -0500

    feat: Add SQS listener entry point with configuration and tests
    In listener, update status with decision result of loan application

---
+++ infrastructure/entry-points/sqs-listener/src/test/java/co/com/crediya/sqs/listener/helper/SQSListenerTest.java
@@ -0,0 +1,78 @@
+package co.com.crediya.sqs.listener.helper;
+
+import co.com.crediya.sqs.listener.SQSProcessor;
+import co.com.crediya.sqs.listener.config.SQSProperties;
+import co.com.crediya.usecase.applyloanapplicationdecision.ApplyLoanApplicationDecisionUseCase;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.util.ReflectionTestUtils;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageResponse;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+class SQSListenerTest {
+
+    @Mock
+    private SqsAsyncClient asyncClient;
+
+    @Mock
+    private SQSProperties sqsProperties;
+
+    @Mock
+    private ApplyLoanApplicationDecisionUseCase applyLoanApplicationDecisionUseCase;
+
+    private ObjectMapper objectMapper;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        objectMapper = new ObjectMapper();
+        sqsProperties = new SQSProperties(
+                "us-east-1",
+                "http://localhost:4566",
+                "http://localhost:4566/00000000000/queueName",
+                20,
+                30,
+                10,
+                1
+        );
+
+        var message = Message.builder().body("message").build();
+        var deleteMessageResponse = DeleteMessageResponse.builder().build();
+        var messageResponse = ReceiveMessageResponse.builder().messages(message).build();
+
+        when(asyncClient.receiveMessage(any(ReceiveMessageRequest.class)))
+                .thenReturn(CompletableFuture.completedFuture(messageResponse));
+        when(asyncClient.deleteMessage(any(DeleteMessageRequest.class)))
+                .thenReturn(CompletableFuture.completedFuture(deleteMessageResponse));
+    }
+
+    @Test
+    void listenerTest() {
+        var sqsListener = SQSListener.builder()
+                .client(asyncClient)
+                .properties(sqsProperties)
+                .processor(new SQSProcessor(applyLoanApplicationDecisionUseCase, objectMapper))
+                .operation("operation")
+                .build();
+
+        Flux<Void> flow = ReflectionTestUtils.invokeMethod(sqsListener, "listen");
+
+        assert flow != null;
+        StepVerifier.create(flow).verifyComplete();
+    }
+}
