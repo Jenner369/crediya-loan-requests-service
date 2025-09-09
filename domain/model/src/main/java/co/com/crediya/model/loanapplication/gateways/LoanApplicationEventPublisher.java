c75a3eee19eb553fced678e84d68d4de87c622f3
Jenner369 <64997441+Jenner369@users.noreply.github.com>
2025-09-08 19:02:35 -0500

    feat: Add publishValidation to LoanApplicationEventPublisher

--- domain/model/src/main/java/co/com/crediya/model/loanapplication/gateways/LoanApplicationEventPublisher.java
+++ domain/model/src/main/java/co/com/crediya/model/loanapplication/gateways/LoanApplicationEventPublisher.java
@@ -1,8 +1,10 @@
 package co.com.crediya.model.loanapplication.gateways;
 
 import co.com.crediya.model.loanapplication.events.LoanApplicationChangedEvent;
+import co.com.crediya.model.loanapplication.events.LoanApplicationValidationEvent;
 import reactor.core.publisher.Mono;
 
 public interface LoanApplicationEventPublisher {
     Mono<Void> publishChange(LoanApplicationChangedEvent event);
+    Mono<Void> publishValidation(LoanApplicationValidationEvent event);
 }
