6ba8b8836f6e89aa38afda48a5fecfdb606943d4
Jenner369 <64997441+Jenner369@users.noreply.github.com>
2025-09-08 19:01:13 -0500

    feat: Add manual review status migration

---
+++ applications/app-service/src/main/resources/db/migration/V5__insert_manual_review_to_statuses.sql
@@ -0,0 +1,2 @@
+INSERT INTO statuses (code, name, description) VALUES
+                                                   ('manual_review', 'Revisión Manual', 'Solicitud que requiere revisión manual adicional');
